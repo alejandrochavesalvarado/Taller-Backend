26d3d68ffe26ff371ab1cea50ba64626896fb24b
Alejandro Chaves <alejandrochavesal@outlook.com>
2024-11-02 23:16:39 -0500

    Adding video-views structures

--- src/main/java/culturemedia/controllers/CultureMediaController.java
+++ src/main/java/culturemedia/controllers/CultureMediaController.java
@@ -1,26 +1,21 @@
-package culturoteca.controllers;
+package culturemedia.controllers;
 
-import java.util.*;
+import java.util.List;
 
-import culturoteca.exception.VideoNotFoundException;
-import culturoteca.model.Video;
-import culturoteca.service.impl.CultureMediaServiceImpl;
+import culturemedia.exception.VideoNotFoundException;
+import culturemedia.model.Video;
+import culturemedia.service.CultureMediaService;
+import culturemedia.service.impl.CultureMediaServiceImpl;
 
 public class CultureMediaController {
 
-    private final CultureMediaServiceImpl cultureMediaService;
-
+    private final CultureMediaService cultureMediaService;
 
     public CultureMediaController(CultureMediaServiceImpl cultureMediaService) {
         this.cultureMediaService = cultureMediaService;
     }
 
-
-    public List<Video> find_allVideos() throws VideoNotFoundException {
-        List<Video> videos = null;
-        videos = cultureMediaService.findAll();
-        return videos;
+    public List<Video> findAllVideos() throws VideoNotFoundException {
+        return cultureMediaService.findAll();
     }
-
-
 }
